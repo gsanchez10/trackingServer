026b79fa73f6861d45e5e3796c70d41de1db28bc
Gabriel Sanchez <gabrielsanchez@Gabriels-MacBook-Pro.local>
2017-08-26 11:19:19 -0600

    cors y hardcoded packs

--- router.js
+++ router.js
@@ -12,6 +12,34 @@ module.exports = function(app) {
 	app.get('/', requireAuth, function(req, res) {
 		res.send({ hi: 'there' });
 	});
+	app.get('/packs', requireAuth, function(req, res) {
+		res.send([
+			{
+				id: 1,
+				courierNumber: '1234',
+				description: 'Zapatos',
+				from: 'Zappos.com',
+				status: 'En transito',
+				charges: 250
+			},
+			{
+				id: 2,
+				courierNumber: 'hsfdg6',
+				description: 'Ropa',
+				from: 'hollisterco.com',
+				status: 'Listo en counter',
+				charges: 76
+			},
+			{
+				id: 3,
+				courierNumber: 'juyytfds5',
+				description: 'Drone',
+				from: 'Amazon.com',
+				status: 'Recibido',
+				charges: 89
+			}
+		]);
+	});
 	app.post('/signin', requireSignin, Authentication.signin);
 	app.post('/signup', Authentication.signup);
 }
